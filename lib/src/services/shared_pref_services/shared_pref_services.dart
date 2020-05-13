08c0f86522fd0302e5de924f0f136383b5b19618
Md Rafat Jamader Maraz <razhossain201@gmail.com>
2020-05-13 13:28:39 +0600

    shared pref added

---
+++ lib/src/services/shared_pref_services/shared_pref_services.dart
@@ -0,0 +1,21 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefServices {
+  static Future<SharedPreferences> get _instance async => _prefsInstance ??= await SharedPreferences.getInstance();
+  static SharedPreferences _prefsInstance;
+
+  // call this method from iniState() function of mainApp().
+  static Future<SharedPreferences> init() async {
+    _prefsInstance = await _instance;
+    return _prefsInstance;
+  }
+
+  static String getString(String key) {
+    return _prefsInstance.getString(key);
+  }
+
+  static Future<bool> setString(String key, String value) async {
+    var prefs = await _instance;
+    return prefs.setString(key, value);
+  }
+}
--- lib/src/services/web_services/blog_api_services.dart
+++ lib/src/services/web_services/blog_api_services.dart
@@ -5,6 +5,7 @@ const baseURL = "https://pirox-foodapi.000webhostapp.com/";
 class BlogApiServices {
   http.Client client = http.Client();
 
+  // get the base url response
   Future<dynamic> fetchBaseUrl() async{
     try{
       var response = await client.get(baseURL);
@@ -18,9 +19,47 @@ class BlogApiServices {
     }
   }
 
-  Future<dynamic> userSignUp() async{
+  // user sign up method
+  Future<dynamic> userSignUp(String name, String email, String password) async{
     try{
-      var response = await client.get(baseURL);
+      var response = await client.post(baseURL+'signUp', body: {
+        'name': name,
+        'email': email,
+        'password': password
+      });
+      if (response.statusCode == 200){
+        return jsonDecode(response.body);
+      } else {
+        return response.statusCode;
+      }
+    } catch (e){
+      print(e.toString());
+    }
+  }
+
+  // user sign in method
+  Future<dynamic> userSignIn(String email, String password) async{
+    try{
+      var response = await client.post(baseURL+'signIn', body: {
+        'email': email,
+        'password': password
+      });
+      if (response.statusCode == 200){
+        return jsonDecode(response.body);
+      } else {
+        return response.statusCode;
+      }
+    } catch (e){
+      print(e.toString());
+    }
+  }
+
+  // get all category
+  Future<dynamic> getCategories() async{
+    try{
+      var response = await client.post(baseURL+'signIn', body: {
+        'api_token': email
+      });
       if (response.statusCode == 200){
         return jsonDecode(response.body);
       } else {
