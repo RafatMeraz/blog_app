7f35141390c4efd20718a0c58197c079be43af31
Md Rafat Jamader Maraz <razhossain201@gmail.com>
2020-05-13 13:35:28 +0600

    post a new blog service added

--- lib/src/services/web_services/blog_api_services.dart
+++ lib/src/services/web_services/blog_api_services.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:blogapp/src/services/shared_pref_services/shared_pref_services.dart';
 import 'package:http/http.dart' as http;
 
 const baseURL = "https://pirox-foodapi.000webhostapp.com/";
@@ -58,7 +59,26 @@ class BlogApiServices {
   Future<dynamic> getCategories() async{
     try{
       var response = await client.post(baseURL+'signIn', body: {
-        'api_token': email
+        'api_token': SharedPrefServices.getString('api_token')
+      });
+      if (response.statusCode == 200){
+        return jsonDecode(response.body);
+      } else {
+        return response.statusCode;
+      }
+    } catch (e){
+      print(e.toString());
+    }
+  }
+
+  // post a new blog
+  Future<dynamic> postNewBlog(String title, String image, String videoUrl, String content) async{
+    try{
+      var response = await client.post(baseURL+'signIn', body: {
+        'api_token': SharedPrefServices.getString('api_token'),
+        'title': title,
+        'url': videoUrl,
+        'description': content
       });
       if (response.statusCode == 200){
         return jsonDecode(response.body);
